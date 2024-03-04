0fd89579446bd6595b6f2186a55606330a380497
Donald Youmans <donald.youmans@unige.ch>
2024-03-04 13:01:26 +0100

    augmentation of code
    installation of numpy in venv
    
    updated pip

--- remember-the-plate.py
+++ remember-the-plate.py
@@ -11,7 +11,6 @@ from sys import platform
 # GUI
 # more modes:
 # sudden-death: go until first miss
-# free practice: go on as long as you want to (maybe option to simply train 1,2 or mixed)
 # survival: ends after 3 misses also gain life after 5 correct in a row?
 # implement international license plates
 # when wrong guess, not only show correct answer but also wrong guess
@@ -71,7 +70,7 @@ class LicensePlate:
         else:
             return self.generate_rnd_plate()
         
-    def generate_plate(self, u_one:int, u_two:int, u_three:int):
+    def generate_spc_plate(self, u_one:int, u_two:int, u_three:int):
         """ 
         generates a license plate with 
         
@@ -104,11 +103,198 @@ class LicensePlate:
         if len(self.lp) <= 10 and len(self.lp) > 0:
             return self.lp
         
-# initialize classes needed
-sys_cmd = SysCmd()
-alphabet = Babel('latin')      
-license_plate = LicensePlate(alphabet)
+class Train(SysCmd,LicensePlate):
+
+    def __init__(self,score,total,miss,alphabet):
+        super().__init__()
+        self.score = score
+        self.total = total
+        self.miss = miss
+        self.alphabet = alphabet
+        self.license_plate = LicensePlate(alphabet)
+
+    def train_one_rnd(self,t,run):
+
+        self.clear_prompt()
+        print('Fertig?')
+        self.pause(1)
+        print('Los!')
+        self.pause(1)
+        
+        lp = self.license_plate.generate_rnd_plate()
+        self.clear_prompt()
+
+        # show license plate and remaining display time
+        for i in range(0,t):
+            print(f'{lp:<20}{t-i:02d}', end='\r')
+            self.pause(1)
+            
+
+        self.clear_prompt()
+
+        kfz = get_answer_one()
+        
     
+        if kfz == lp.lower().replace('-','').replace(' ',''):
+            run += 1
+            print('Korrekt!')
+            print(f'Score: {self.total-self.miss}/{self.total}')
+            if len(self.score) == 1:
+                print(f'Highscore: {run}\n')
+            else:
+                print(f'Highscore: {max(self.score)}\n')
+            return run, self.miss, self.score
+   
+        else:
+            self.miss += 1
+            self.score.append(run)
+            print(f'Schade! Richtig wäre gewesen')
+            print(f'\n{lp}\n')
+            print('Versuch es nochmal!')
+            print(f'Score: {self.total-self.miss}/{self.total}')
+            print(f'Highscore: {max(self.score)}')
+            return run, self.miss, self.score
+        
+    def train_one_spc(self,t,run,unit_one,unit_two,unit_three):
+        self.clear_prompt()
+        print('Fertig?')
+        self.pause(1)
+        print('Los!')
+        self.pause(1)
+        
+        lp = self.license_plate.generate_spc_plate(unit_one,unit_two,unit_three)
+        self.clear_prompt()
+
+        # show license plate and remaining display time
+        for i in range(0,t):
+            print(f'{lp:<20}{t-i:02d}', end='\r')
+            self.pause(1)
+            
+        self.clear_prompt()
+
+        kfz = get_answer_one()
+    
+        if kfz == lp.lower().replace('-','').replace(' ',''):
+            run += 1
+            print('Korrekt!')
+            print(f'Score: {self.total-self.miss}/{self.total}')
+            if len(self.score) == 1:
+                print(f'Highscore: {run}\n')
+            else:
+                print(f'Highscore: {max(self.score)}\n')
+            return run, self.miss, self.score
+   
+        else:
+            self.miss += 1
+            self.score.append(run)
+            print(f'Schade! Richtig wäre gewesen')
+            print(f'\n{lp}\n')
+            print('Versuch es nochmal!')
+            print(f'Score: {self.total-self.miss}/{self.total}')
+            print(f'Highscore: {max(self.score)}')
+            return run, self.miss, self.score
+
+
+    def train_two_rnd(self,t,run):
+        self.clear_prompt()
+        print('Fertig?')
+        self.pause(1)
+        print('Los!')
+        self.pause(1)
+        self.clear_prompt()
+
+        lp = [self.license_plate.generate_rnd_plate(),self.license_plate.generate_rnd_plate()]
+        for i in range(0,t): 
+            print(f'{lp[1]:<20} {t-i:02d}')
+            print(lp[0])
+            self.pause(1)
+            self.clear_prompt()
+    
+        lpp = [s.lower().replace('-','').replace(' ','') for s in lp]
+
+        kfz = [s for s in get_answer_two()]
+
+        if kfz[0] in lpp and kfz[1] in lpp:
+            run += 2
+            print('Stark! 2/2 korrekt!')
+            print(f'Score: {run}\n')
+            self.pause(1)
+            return run, self.miss, self.score
+     
+        
+        elif (kfz[0] in lpp and kfz[1] not in lpp) or (kfz[0] not in lpp and kfz[1] in lpp):
+            run += 1
+            self.miss += 1
+            print('Fast! 1/2 richtig!')
+            if kfz[1] not in lpp:
+                print(f'Das zweite Kennzeichen wäre {kfz[0]} gewesen.')
+            else:
+                print(f'Das zweite Kennzeichen wäre {kfz[1]} gewesen.')
+            print(f'Score: {self.total-self.miss}/{self.total}\n')
+            self.pause(1)
+            return run, self.miss, self.score
+   
+        else:
+            self.miss += 2
+            print('Schade. Die Kennzeichen waren')
+            print('')
+            print(lp[0])
+            print(lp[1])
+            print('')
+            print('Gleich nochmal!')
+            print(f'Score: {self.total-self.miss}/{self.total}')
+            print(f'Highscore: {max(self.score)}')
+            return run, self.miss, self.score
+    
+    def train_two_spc(self,t,run,unit_one,unit_two,unit_three):
+        self.clear_prompt()
+        print('Fertig?')
+        self.pause(1)
+        print('Los!')
+        self.pause(1)
+        self.clear_prompt()
+
+        lp = [self.license_plate.generate_spc_plate(unit_one,unit_two,unit_three),self.license_plate.generate_spc_plate(unit_one,unit_two,unit_three)]
+        for i in range(0,t): 
+            print(f'{lp[1]:<20} {t-i:02d}')
+            print(lp[0])
+            self.pause(1)
+            self.clear_prompt()
+    
+        lpp = [s.lower().replace('-','').replace(' ','') for s in lp]
+
+        kfz = [s for s in get_answer_two()]
+
+        if kfz[0] in lpp and kfz[1] in lpp:
+            run += 2
+            print('Stark! 2/2 korrekt!')
+            print(f'Score: {run}\n')
+            return run, self.miss, self.score
+     
+        
+        elif (kfz[0] in lpp and kfz[1] not in lpp) or (kfz[0] not in lpp and kfz[1] in lpp):
+            run += 1
+            self.miss += 1
+            print('Fast! 1/2 richtig!')
+            if kfz[1] not in lpp:
+                print(f'Das zweite Kennzeichen wäre {kfz[0]} gewesen.')
+            else:
+                print(f'Das zweite Kennzeichen wäre {kfz[1]} gewesen.')
+            print(f'Score: {self.total-self.miss}/{self.total}\n')
+            return run, self.miss, self.score
+   
+        else:
+            self.miss += 2
+            print('Schade. Die Kennzeichen waren')
+            print('')
+            print(lp[0])
+            print(lp[1])
+            print('')
+            print('Gleich nochmal!')
+            print(f'Score: {self.total-self.miss}/{self.total}')
+            print(f'Highscore: {max(self.score)}')
+            return run, self.miss, self.score
+        
 def get_kfz(str):
     """ get a guess for a license plate from user """
     try:
@@ -116,7 +302,7 @@ def get_kfz(str):
     except AttributeError: # if one wants to skip the license plate, one is asked to  hit enter returning a NoneType objet. This raises an AttributeError in get_answer_one().lower()
         kfz = 'skipped'
     
-    if len(kfz) < 4 and len(kfz) > 0 or len(kfz) > 8:
+    if len(kfz) < 3 and len(kfz) > 0 or len(kfz) > 8:
         print('\nBitte gib ein gültiges Kennzeichen ein.')
         print('Ein Kennzeichen muss mindestens 6 Zeichen beinhalten.')
         print('Wenn du dieses Kennzeichen überspringen möchtest, tippe Enter \n')
@@ -183,6 +369,7 @@ def test_one(t,run,score,mode):
     for i in range(0,t):
         print(f'{lp:<20}{t-i:02d}', end='\r')
         sys_cmd.pause(1)
+        
 
     sys_cmd.clear_prompt()
 
@@ -210,7 +397,7 @@ def test_one(t,run,score,mode):
         elif mode == 'test':
             return run, False
     
-def test_two(t,run,score):
+def test_two(t,run,score,mode):
     global total
     global miss
 
@@ -231,8 +418,6 @@ def test_two(t,run,score):
         sys_cmd.clear_prompt()
  
     lpp = [s.lower().replace('-','').replace(' ','') for s in lp]
-    sys_cmd.pause(t)
-    sys_cmd.clear_prompt()
 
     kfz = [s for s in get_answer_two()]
 
@@ -241,7 +426,11 @@ def test_two(t,run,score):
         print('Stark! 2/2 korrekt!')
         print(f'Score: {run}\n')
         sys_cmd.pause(1)
-        return run, True
+        if mode == 'training':
+            return run
+        elif mode == 'test':
+            return run, False
+    
     elif (kfz[0] in lpp and kfz[1] not in lpp) or (kfz[0] not in lpp and kfz[1] in lpp):
         run += 1
         miss += 1
@@ -252,8 +441,10 @@ def test_two(t,run,score):
             print(f'Das zweite Kennzeichen wäre {kfz[1]} gewesen.')
         print(f'Score: {total-miss}/{total}\n')
         sys_cmd.pause(1)
-        
-        return run, True
+        if mode == 'training':
+            return run
+        elif mode == 'test':
+            return run, False
     else:
         miss += 2
         print('Schade. Die Kennzeichen waren')
@@ -264,7 +455,10 @@ def test_two(t,run,score):
         print('Gleich nochmal!')
         print(f'Score: {total-miss}/{total}')
         print(f'Highscore: {max(score)}')
-        return run, False
+        if mode == 'training':
+            return run
+        elif mode == 'test':
+            return run, False
 
  
 def test(t,score):
@@ -292,30 +486,58 @@ def get_time(str):
     except ValueError:
         return get_time(input('Bitte gebe eine ganze Zahl ein: '))
 
-def training(t,run,score,option):
+def training(t,run,score,option,choice,mode_obj,unit_one,unit_two,unit_three):
+    # under construction
+    train = mode_obj
 
     if option == 1:
-        global total
+        print('\nGo! (enter)')
+        print('Hauptmenu (quit)')
+        go_or_quit = input()
         
+        if  go_or_quit == '':
+            # run = test_one(t,run,score,'training')
+            # training(t,run,score,1)
+            train.total += 1
+            if choice == 1:
+                train.run, train.miss, train.score = train.train_one_rnd(t,run)
+                training(t,run,score,1,1,train,unit_one,unit_two,unit_three)
+            elif choice == 2:
+                train.run, train.miss, train.score = train.train_one_spc(t,run,unit_one,unit_two,unit_three)
+                training(t,run,score,1,2,train,unit_one,unit_two,unit_three)
+         
+        elif go_or_quit == 'quit':
+            return False
+              
+
+    if option == 2:
         print('\nGo! (enter)')
         print('Hauptmenu (quit)')
         go_or_quit = input()
         if  go_or_quit == '':
-            run = test_one(t,run,score,'training')
-            training(t,run,score,1)
+            # run = test_two(t,run,score,'training')
+            # training(t,run,score,2)
+            train.total += 2
+            if choice == 1:
+                train.run, train.miss, train.score = train.train_two_rnd(t,run,unit_one,unit_two,unit_three)
+                training(t,run,score,2,1,train,unit_one,unit_two,unit_three)
+            elif choice == 2:
+                train.run, train.miss, train.score = train.train_two_spc(t,run,unit_one,unit_two,unit_three)
+                training(t,run,score,2,2,train,unit_one,unit_two,unit_three)
 
         elif go_or_quit == 'quit':
             return False
-                
-
-    if option == 2:
-        # train_two(score,t)
-        print('under construction')
-
+        
 
 def main(): 
     global miss
     global total
+
+    # initialize classes needed
+    sys_cmd = SysCmd()
+    alphabet = Babel('latin')      
+    license_plate = LicensePlate(alphabet)
+    train = Train([0],0,0,alphabet)
     
 
     while True:
@@ -359,16 +581,51 @@ def main():
 
             sys_cmd.clear_prompt()
             print('Willkommen im Übungslager.\n')
-            print('Was würdest Du gerne Trainieren?')
+            print('Was würdest Du gerne trainieren?')
             print('1. 1 Kfz-Kennzeichen')
             print('2. 2 Kfz-Kennzeichen\n')
             try:
                 option = int(input('Option: '))
             except ValueError:
                 print('Bitte gebe 1 oder 2 ein.')
+
             t = get_time(input('Wie wie viele Sekunden möchtest Du Zeit haben? '))
 
-            training(t,run,score,option)
+            print('\nWas möchtest Du üben?')
+            print('1. Zufälliges Kennzeichen')
+            print('2. Sepzifische Konfigurationen\n')
+
+            
+            try:
+                choice = int(input())
+            except TypeError:
+                print('Bitte einen integer Eingeben')
+                # need to outsource 
+       
+
+            if choice == 2:
+                try:
+                    unit_one = int(input('\nWie viele Buchstaben soll es in der ersten Einheit geben (1/2/3)? '))
+                except TypeError:
+                    print('Bitte einen integer Eingeben')
+                    # need to outsource 
+
+                try:
+                    unit_two = int(input('Wie viele Buchstaben soll es in der zweiten Einheit geben (1/2)? '))
+                except TypeError:
+                    print('Bitte einen integer Eingeben')
+                    # need to outsource 
+
+                try:
+                    unit_three = int(input('Wie viele Ziffern soll das Kennzeichen haben (1/2/3/4)? '))
+                except TypeError:
+                    print('Bitte einen integer Eingeben')
+                    # need to outsource
+
+                training(t,run,score,option,choice,train,unit_one,unit_two,unit_three)
+
+            else:
+                training(t,run,score,option,choice,train,0,0,0)
     
         
     
